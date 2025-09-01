2673c518b7adb153a2d632031c4d860225cc8628
yechan <yechan24680@gmail.com>
2025-09-02 07:10:03 +0900

    first commit

---
+++ src/main/java/yechan2468/inflearn_spring_core_basic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package yechan2468.inflearn_spring_core_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yechan2468/inflearn_spring_core_basic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package yechan2468.inflearn_spring_core_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
