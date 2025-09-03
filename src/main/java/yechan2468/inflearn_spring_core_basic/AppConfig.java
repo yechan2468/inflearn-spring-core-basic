abba2c3510fcfbf90efd0fcbad880de62d086777
yechan <yechan24680@gmail.com>
2025-09-03 11:01:37 +0900

    section5 26

--- src/main/java/yechan2468/inflearn_spring_core_basic/AppConfig.java
+++ src/main/java/yechan2468/inflearn_spring_core_basic/AppConfig.java
@@ -1,5 +1,7 @@
 package yechan2468.inflearn_spring_core_basic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import yechan2468.inflearn_spring_core_basic.discount.DiscountPolicy;
 import yechan2468.inflearn_spring_core_basic.discount.RateDiscountPolicy;
 import yechan2468.inflearn_spring_core_basic.member.MemberRepository;
@@ -9,20 +11,25 @@ import yechan2468.inflearn_spring_core_basic.member.MemoryMemberRepository;
 import yechan2468.inflearn_spring_core_basic.order.OrderService;
 import yechan2468.inflearn_spring_core_basic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
