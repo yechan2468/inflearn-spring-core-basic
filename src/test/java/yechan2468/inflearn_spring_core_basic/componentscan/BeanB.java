994ef86ded5552018122045a67d1d613c4bc1c98
yechan <yechan24680@gmail.com>
2025-09-04 05:25:24 +0900

    section7 42

---
+++ src/test/java/yechan2468/inflearn_spring_core_basic/componentscan/BeanB.java
@@ -0,0 +1,5 @@
+package yechan2468.inflearn_spring_core_basic.componentscan;
+
+@MyExcludeComponent
+public class BeanB {
+}
---
+++ src/test/java/yechan2468/inflearn_spring_core_basic/componentscan/MyIncludeComponent.java
@@ -0,0 +1,10 @@
+package yechan2468.inflearn_spring_core_basic.componentscan;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
