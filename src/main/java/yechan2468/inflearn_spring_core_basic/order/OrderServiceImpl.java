8884f3167cb1bb50c8e6edcf59f50364841fdbee
yechan <yechan24680@gmail.com>
2025-09-04 09:37:31 +0900

    section7 47

--- src/main/java/yechan2468/inflearn_spring_core_basic/order/OrderServiceImpl.java
+++ src/main/java/yechan2468/inflearn_spring_core_basic/order/OrderServiceImpl.java
@@ -1,22 +1,17 @@
 package yechan2468.inflearn_spring_core_basic.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import yechan2468.inflearn_spring_core_basic.discount.DiscountPolicy;
 import yechan2468.inflearn_spring_core_basic.member.Member;
 import yechan2468.inflearn_spring_core_basic.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
